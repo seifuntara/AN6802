356d041c860ef30bd04f6b02e26218780ac4b5cf
seifuntara <43272018+seifuntara@users.noreply.github.com>
2026-03-05 12:27:20 +0000

    2

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/ethics",methods=["GET","POST"])
+def ethics():
+    return(render_template("ethics.html"))
+
+@app.route("/correct",methods=["GET","POST"])
+def correct():
+    return(render_template("correct.html"))
+
+@app.route("/wrong",methods=["GET","POST"])
+def wrong():
+    return(render_template("wrong.html"))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)
