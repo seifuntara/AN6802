e7057d3abd287b94ae5a61573eb5b7f3bc62c262
seifuntara <43272018+seifuntara@users.noreply.github.com>
2026-03-13 12:27:06 +0000

    hi

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
 import joblib
+import os
+from groq import Groq
+
+
 
 app = Flask(__name__)
+
+client = Groq()
 
 
 @app.route("/",methods=["GET","POST"])
@@ -34,6 +40,25 @@ def foodexp():
     model = joblib.load('foodexp.pkl')
     r = model.predict([[salary]])
     return(render_template("foodexp.html",r=r))
+
+@app.route("/chatbot", methods=["GET","POST"])
+def chatbot():
+    r = None
+    query = request.form.get("query")
+
+    if query:
+        completion = client.chat.completions.create(
+            model="openai/gpt-oss-20b",
+            messages=[
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ]
+        )
+        r = completion.choices[0].message.content
+
+    return render_template("chatbot.html", r=r)
 
 if __name__ == "__main__":
     app.run()
